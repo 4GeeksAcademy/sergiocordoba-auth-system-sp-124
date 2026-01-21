8bb74e8d4010ef5da91f058361def888e655a45e
Sergio Cordoba <sergiocordoba246@gmail.com>
2026-01-21 11:56:07 +0000

    Y si no entiendo?

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from .models import User, db
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +13,27 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/sign_in', methods=['POST', 'GET'])
+def handle_sign_in():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Crear Usuario"
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/login', methods=['GET'])
+def handle_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)    
+
+    User = db.session.execute(db.select('User').where(User.password == password, User.email == email, User.is_active))
+
+
+    response_body = {
+        "message": "Iniciar sesión"
+    }
+
+    return jsonify(response_body), 200
+
